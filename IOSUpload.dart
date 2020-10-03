326a38264f7445bf76ac93cc87d722771cfaa77a
hardikkumbhaniinfilon <63768193+hardikkumbhaniinfilon@users.noreply.github.com>
2020-10-03 18:26:14 +0530

    Create IOSUpload.dart

---
+++ IOSUpload.dart
@@ -0,0 +1,9 @@
+First all of 
+1. Create certificate and export in keychain (Two certificate Development and Distribution)
+With this you have to create csr file from keychain
+2. Create app bundle in indentifiers section
+3. If You want to test ios app to client then add udid to device section or add email id.
+4. After that create devlopment and production profile
+5. Create a app in applestoreconnect and 
+and then open workspace in xcode
+Please remember open every certificate once with double click so that it will store in keychain
